47ec6ec106b5a4c157fd805ea4b90c80bd898431
theskelinen <ssahlman1@gmail.com>
2023-10-03 19:44:52 +0300

    step2

--- Osa1/kurssitiedot/src/App.jsx
+++ Osa1/kurssitiedot/src/App.jsx
@@ -18,15 +18,9 @@ const App = () => {
   const Content = (props) => {
     return (
       <div>
-        <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-        {props.part2} {props.exercises2}
-        </p>
-        <p>
-          {props.part3} {props.exercises3}
-        </p>
+        <Part part={props.part1} exercises={props.exercises1}/>
+        <Part part={props.part2} exercises={props.exercises2}/>
+        <Part part={props.part3} exercises={props.exercises3}/>
       </div>
     )
   }
@@ -35,6 +29,14 @@ const App = () => {
     return (
       <div>
          <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      </div>
+    )
+  }
+
+  const Part = (props) => {
+    return (
+      <div>
+         <p>{props.part} {props.exercises}</p>
       </div>
     )
   }
