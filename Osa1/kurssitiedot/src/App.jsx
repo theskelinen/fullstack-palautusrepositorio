33c4a357620052a67eb7c9eadc90058ad630c11a
theskelinen <ssahlman1@gmail.com>
2023-10-03 19:40:17 +0300

    step1 fixed

--- Osa1/kurssitiedot/src/App.jsx
+++ Osa1/kurssitiedot/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   const Header = (props) => {
     return (
       <div>
-         <h1>{course}</h1>
+         <h1>{props.course}</h1>
       </div>
     )
   }
@@ -19,13 +19,13 @@ const App = () => {
     return (
       <div>
         <p>
-          {part1} {exercises1}
+          {props.part1} {props.exercises1}
         </p>
         <p>
-          {part2} {exercises2}
+        {props.part2} {props.exercises2}
         </p>
         <p>
-          {part3} {exercises3}
+          {props.part3} {props.exercises3}
         </p>
       </div>
     )
@@ -34,15 +34,16 @@ const App = () => {
   const Total = (props) => {
     return (
       <div>
-         <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+         <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
       </div>
     )
   }
 
+
   return (
     <div>
      <Header course={course} />
-     <Content part1={part1} part2={part2} part3={part3}  />
+     <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
