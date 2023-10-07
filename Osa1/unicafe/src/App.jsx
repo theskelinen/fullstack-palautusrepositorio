20e221d5d361f2bf7ac0c970bca13f8e31012c0a
theskelinen <ssahlman1@gmail.com>
2023-10-07 20:51:29 +0300

    step2

--- Osa1/unicafe/src/App.jsx
+++ Osa1/unicafe/src/App.jsx
@@ -14,7 +14,6 @@ const Button = (props) => {
   return (
     <button onClick={props.handleClick}>
       {props.text}
-      {props.loki}
     </button>
   )
 }
@@ -24,26 +23,48 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [points, setPoints] = useState(0)
+  
+  const positives = () => good/total + " %"
 
   return (
     <div>
       <Display text={<h1>give feedback</h1>}/>
       <Button
-        handleClick={() => setGood(good + 1)}
+        handleClick={() => {
+          setGood(good + 1)
+          setTotal(total + 1)
+          setPoints(points + 1)
+        }
+      }
         text='good'
       />
       <Button
-        handleClick={() => setNeutral(neutral + 1)}
+        handleClick={() => {
+          setNeutral(neutral + 1)
+          setTotal(total + 1)
+          setPoints(points + 0)
+        }
+      }
         text='neutral'
       />
       <Button
-        handleClick={() => setBad(bad + 1)}
+        handleClick={() => {
+          setBad(bad + 1)
+          setTotal(total + 1)
+          setPoints(points - 1)
+        }
+      }
         text='bad'
       />
       <Display text={<h1>statistics</h1>}/>
       <Display text={good} name="good "/>
       <Display text={neutral} name="neutral "/>
       <Display text={bad} name="bad "/>
+      <Display text={total} name="all "/>
+      <Display text={points/total} name="average "/>
+      <Display text={positives()} name="positive "/>
     </div>
   )
 }
