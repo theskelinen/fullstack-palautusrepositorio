a754f09e8a31faaeac94ca2f5b764c0dbcc51cf8
theskelinen <ssahlman1@gmail.com>
2023-12-20 14:20:40 +0200

    puhelinluettelo step1

--- Osa_2/puhelinluettelo/src/App.jsx
+++ Osa_2/puhelinluettelo/src/App.jsx
@@ -1,24 +1,53 @@
 import { useState } from 'react'
 
+const Person = ({ person }) => 
+  <p>
+    {person.name}
+  </p>
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: "Arto Hellas" }
   ]) 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState("")
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName("")
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: 
+          <input
+            value={newName} 
+            onChange={handleNameChange} 
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map(person =>
+          <Person key={person.name} person={person} />
+        )}
+      </div>
     </div>
   )
 
