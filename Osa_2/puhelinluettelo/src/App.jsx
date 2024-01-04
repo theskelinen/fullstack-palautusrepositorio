ad4562749495431e7ef2b8590014584186a1872e
theskelinen <ssahlman1@gmail.com>
2024-01-04 20:53:56 +0200

    puhelinluettelo step6

--- Osa_2/puhelinluettelo/src/App.jsx
+++ Osa_2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({ person }) => 
   <p>
@@ -43,12 +44,8 @@ const PersonForm = (props) =>
 </form>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
+
   const [newName, setNewName] = useState("")
 
   const [newNumber, setNewNumber] = useState("")
@@ -88,6 +85,16 @@ const App = () => {
   const personsToShow = newFilter === ""
     ? persons
     : persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   return (
     <div>
